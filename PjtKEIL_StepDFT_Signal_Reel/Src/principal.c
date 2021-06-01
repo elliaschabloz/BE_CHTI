09c135afd73243fea0d5e0cafc6fbb649b6cf548
Ellias Chabloz <chabloz@insat.insa-toulouse.fr>
2021-06-01 18:18:56 +0200

    maj

--- PjtKEIL_StepDFT_Signal_Reel/Src/principal.c
+++ PjtKEIL_StepDFT_Signal_Reel/Src/principal.c
@@ -10,6 +10,13 @@ short int tab[64];
 short int tab_ech[64];
 short int tab_score[4];
 
+int f1 = 85000;
+int f2 = 90000;
+int f3 = 95000;
+int f4 = 100000;
+int f5 = 115000;
+int f6 = 120000;
+
 
 int DFT_ModuleAuCarre( short int * Signal64ech, char k){
 	return DFT(Signal64ech, k, &TabCos) + DFT(Signal64ech, k, &TabSin);
@@ -61,7 +68,15 @@ Init_ADC1_DMA1( 0, tab_ech );
 
 while	(1)
 	{
-		
+		if(tab[f1/5000] >= 100){
+			tab_score[0] += 1;
+		} else if(tab[f2/5000] >= 100){
+			tab_score[1] += 1;
+		} else if(tab[f3/5000] >= 100){
+			tab_score[2] += 1;
+		} else if(tab[f4/5000] >= 100){
+			tab_score[3] += 1;
+		}
 	}
 }
 
