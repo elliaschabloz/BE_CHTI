37eb8c4e39fe87fb4781fa8e741cd4b93e5a2478
Ellias Chabloz <chabloz@insat.insa-toulouse.fr>
2021-06-01 17:36:07 +0200

    maj

--- PjtKEIL_StepDFT_Signal_Reel/Src/principal.c
+++ PjtKEIL_StepDFT_Signal_Reel/Src/principal.c
@@ -7,20 +7,27 @@ extern short int TabCos;
 extern short int TabSin;
 
 short int tab[64];
+short int tab_ech[64];
 
-/* Faire la fct en ASM avec une boucle while
-La fct C appele la fct ASM
-Le 2pi/M va disparaitre
-*/
+int DFT_ModuleAuCarre( short int * Signal64ech, char k){
+	return DFT(Signal64ech, k, &TabCos) + DFT(Signal64ech, k, &TabSin);
+}
+
+void callbackSystick(void){
+	Start_DMA1(64);
+	Wait_On_End_Of_DMA1();
+	Stop_DMA1;
+	for(int i=0; i<64; i++){
+		tab[i] = DFT_ModuleAuCarre( tab_ech, i);
+	}
+}
+
+
 void test( ){
 	for(int i=0; i<64; i++){
 		tab[i] = DFT(&LeSignal, i, &TabSin) + DFT(&LeSignal, i, &TabCos);
 	}
 
-}
-
-int DFT_ModuleAuCarre( short int * Signal64ech, char k){
-	return DFT(&LeSignal, k, &TabCos) + DFT(&LeSignal, k, &TabSin);
 }
 	
 int main(void)
@@ -35,15 +42,16 @@ CLOCK_Configure();
 
 /// Timer Systick ///
 
+	//
 Systick_Period_ff( 360000 );
-Systick_Prio_IT( 2, &Systick_Period_ff );
+Systick_Prio_IT( 2, &callbackSystick );
 SysTick_On;
 SysTick_Enable_IT;
 	
 Init_TimingADC_ActiveADC_ff( ADC1, 72 );
 Single_Channel_ADC( ADC1, 2 );	
 Init_Conversion_On_Trig_Timer_ff( ADC1, TIM2_CC2, 225 );
-Init_ADC1_DMA1( 0, tab );
+Init_ADC1_DMA1( 0, tab_ech );
 
 //============================================================================	
 	
